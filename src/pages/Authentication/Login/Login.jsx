7a9da2edad96b6445abf1afa45802134e5486ecc
SenPatthamwan <sen.patthamwan@gmail.com>
2024-01-14 13:23:15 +0700

    Modified layout login page & remove outline input field onFocus

--- src/pages/Authentication/Login/Login.jsx
+++ src/pages/Authentication/Login/Login.jsx
@@ -3,18 +3,13 @@ import {
     Container,
     Grid,
     Typography,
-    createTheme,
-    IconButton,
     ThemeProvider,
-    Stack
   } from "@mui/material"
   
 import LoginImg from "/images/login.jpg"
 import { theme } from "../../../theme"
 import { Link } from "react-router-dom"
-  
-  // const defaultTheme = createTheme(theme);
-  
+    
   const Login = () => {
   
     return (
@@ -32,8 +27,6 @@ import { Link } from "react-router-dom"
               width: 760,
               height: 480,
               borderRadius: '20px',
-              // border: 2,
-              // borderColor: 'primary.main',
               bgcolor: 'primary.white',
               display: 'grid', 
               gridTemplateColumns: 'repeat(2, 1fr)',
@@ -59,65 +52,64 @@ import { Link } from "react-router-dom"
                 borderRadius: '18px 0 0 18px',
               }}
             />
-  
-  
-
-
 
             <Grid item xs={false} sm={4} md={7} 
               sx={{
                 fontFamily: 'Inter',
                 textAlign: 'center',
                 alignItems: 'center',
+                display: 'flex',
                 border: 2,
                 borderLeft: 0,
                 borderColor: 'primary.main',
                 borderRadius: '0 18px 18px 0',
-                // height: 480,
                 p: 4,
+                
               }}>
-                <Typography variant="h5" component="h1" >
-                  Login
-                </Typography>
-              <form className="max-w-sm mx-auto">
-                <div className="mb-5">
-                  <label htmlFor="email" 
-                         className="text-left block mb-3 mt-6 text-sm text-gray-900 dark:text-white"
-                  >
-                         Username or email
-                  </label>
-                  <input type="email" id="email" 
-                         className="pl-5 placeholder-white border-transparent rounded-4xl bg-blue bg-gray-50 text-black text-sm block w-full p-2.5" 
-                         placeholder="Username or email" required />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="password" 
-                         className="text-left block mb-3 text-sm text-gray-900"
-                  >
-                    Password
-                  </label>
-                  <input type="password" id="password" 
-                         className="pl-5 placeholder-white border-transparent rounded-4xl bg-blue text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" 
-                         placeholder="Password" required />
-                </div>
-                <div className="flex items-start mb-10">
-                  <p className="text-pink text-sm text-gray-900 dark:text-gray-300">
-                    <Link to="/forgot-password">Forgot password?</Link>
-                  </p>
-                </div>
-                <div className="mb-5">
-                  
-                    <button type="submit" 
-                            className="rounded-4xl text-white bg-pink text-sm w-full px-5 py-2.5 text-center"
-                            >
-                            <Link to="/dashboard">
-                              Login
-                            </Link>
-                    </button>
-                  
-                </div>
-                <p className="text-sm text-gray-900 dark:text-gray-300">Don’t have an account ? <Link to="/signup" className="text-pink">Sign up</Link></p>
-              </form>
+                <Box sx={{
+                  width: '100%',
+                }}>
+                  <Typography variant="h5" component="h1" >
+                    Login
+                  </Typography>
+                  <form className="max-w-sm mx-auto">
+                    <div className="mb-5">
+                      <label htmlFor="email" 
+                            className="text-left block mb-3 mt-6 text-sm"
+                      >
+                            Username or email
+                      </label>
+                      <input type="email" id="email" 
+                            className="outline-0 pl-5 placeholder-white border-transparent rounded-4xl bg-blue text-black text-sm block w-full p-2.5" 
+                            placeholder="Username or email" required />
+                    </div>
+                    <div className="mb-3">
+                      <label htmlFor="password" 
+                            className="text-left block mb-3 text-sm"
+                      >
+                        Password
+                      </label>
+                      <input type="password" id="password" 
+                            className="outline-0 pl-5 placeholder-white border-transparent rounded-4xl bg-blue text-sm block w-full p-2.5" 
+                            placeholder="Password" required />
+                    </div>
+                    <div className="flex items-start mb-10">
+                      <p className="text-pink text-sm">
+                        <Link to="/forgot-password">Forgot password?</Link>
+                      </p>
+                    </div>
+                    <div className="mb-5">
+                        <button type="submit" 
+                                className="rounded-4xl text-white bg-pink text-sm w-full px-5 py-2.5 text-center"
+                                >
+                                <Link to="/dashboard">
+                                  Login
+                                </Link>
+                        </button>
+                    </div>
+                    <p className="text-sm text-gray-900 dark:text-gray-300">Don’t have an account ? <Link to="/signup" className="text-pink">Sign up</Link></p>
+                  </form>
+              </Box>
             </Grid>
           </Box>
         </Container>
