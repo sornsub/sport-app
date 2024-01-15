155f6af13817c9861fcd08ef849a969416bd31ae
ekachai39_8re <146946429+ekachaikeaw@users.noreply.github.com>
2024-01-15 15:34:46 +0700

    landingPage: add path on main and landingPageDesk and mobile template

--- src/main.jsx
+++ src/main.jsx
@@ -1,67 +1,110 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
 // font inter
-import '@fontsource/inter/300.css';
-import '@fontsource/inter/400.css';
-import '@fontsource/inter/500.css';
-import '@fontsource/inter/700.css';
+import "@fontsource/inter/300.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/700.css";
 
-import { createBrowserRouter, RouterProvider, } from "react-router-dom";
-import Login from './pages/Login/Login.jsx';
-import SignUp from './pages/SignUp/SignUp.jsx';
-import ActivityType from './pages/ActivityType/ActivityType.jsx';
-import Dashboard from './pages/Dashboard/Dashboard.jsx';
-import CreateExercise from './pages/Exercise/CreateExercise/CreateExercise.jsx';
-import EditExercise from './pages/Exercise/EditExercise/EditExercise.jsx';
-import DeleteExercise from './pages/Exercise/DeleteExercise/DeleteExercise.jsx';
-import History from './pages/History/History.jsx';
-import LandingPage from './pages/LandingPage/LandingPage.jsx';
-
-
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/Login/Login.jsx";
+import SignUp from "./pages/SignUp/SignUp.jsx";
+import ActivityType from "./pages/ActivityType/ActivityType.jsx";
+import Dashboard from "./pages/Dashboard/Dashboard.jsx";
+import CreateExercise from "./pages/Exercise/CreateExercise/CreateExercise.jsx";
+import EditExercise from "./pages/Exercise/EditExercise/EditExercise.jsx";
+import DeleteExercise from "./pages/Exercise/DeleteExercise/DeleteExercise.jsx";
+import History from "./pages/History/History.jsx";
+import LandingPage from "./pages/LandingPage/LandingPage.jsx";
+import LandingPage2 from "./pages/LandingPage/LandingPage2.jsx";
+import LandingPage3 from "./pages/LandingPage/LandingPage3.jsx";
+import LandingPage4 from "./pages/LandingPage/LandingPage4.jsx";
+import LandingPageStarted from "./pages/LandingPage/LandingPageStarted.jsx";
+import LandingPageDesk1 from "./pages/LandingPage/LandingPageDesk1.jsx";
+import LandingPageDesk2 from "./pages/LandingPage/LandingPageDesk2.jsx";
+import LandingPageDesk3 from "./pages/LandingPage/LandingPageDesk3.jsx";
+import LandingPageDesk4 from "./pages/LandingPage/LandingPageDesk4.jsx";
+import LandingPageDesk5 from "./pages/LandingPage/LandingPageDesk5.jsx";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <LandingPage />
+    path: "/",
+    element: <LandingPage />,
   },
   {
-    path: '/login',
-    element: <Login />
+    path: "/landingPage2",
+    element: <LandingPage2 />,
   },
   {
-    path: '/signup',
-    element: <SignUp />
+    path: "/landingPage3",
+    element: <LandingPage3 />,
   },
   {
-    path: '/activity-type',
-    element: <ActivityType />
+    path: "/landingPage4",
+    element: <LandingPage4 />,
   },
   {
-    path: '/dashboard',
-    element: <Dashboard />
+    path: "/landingPageStarted",
+    element: <LandingPageStarted />,
   },
   {
-    path: '/exercise-create',
-    element: <CreateExercise />
+    path: "/landingPageDesk1",
+    element: <LandingPageDesk1 />,
   },
   {
-    path: '/exercise-edit',
-    element: <EditExercise />
+    path: "/landingPageDesk2",
+    element: <LandingPageDesk2 />,
   },
   {
-    path: '/exercise-delete',
-    element: <DeleteExercise />
+    path: "/landingPageDesk3",
+    element: <LandingPageDesk3 />,
   },
   {
-    path: '/history',
-    element: <History />
-  }
-])
+    path: "/landingPageDesk4",
+    element: <LandingPageDesk4 />,
+  },
+  {
+    path: "/landingPageDesk5",
+    element: <LandingPageDesk5 />,
+  },
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+  {
+    path: "/activity-type",
+    element: <ActivityType />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+  {
+    path: "/exercise-create",
+    element: <CreateExercise />,
+  },
+  {
+    path: "/exercise-edit",
+    element: <EditExercise />,
+  },
+  {
+    path: "/exercise-delete",
+    element: <DeleteExercise />,
+  },
+  {
+    path: "/history",
+    element: <History />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
-)
+);
--- src/pages/LandingPage/LayoutDesk.jsx
+++ src/pages/LandingPage/LayoutDesk.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
-import Navbar from './Nav'
+import React from "react";
+import Navbar from "./Nav";
+import { Box } from "@mui/material";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
 
-export const LayoutDesk = ({children}) => {
+export const LayoutDesk = ({ children }) => {
   return (
-    <Navbar>
-        {children}
-    </Navbar>
-  )
-}
+    <Box className="container h-screen bg-red-400 text-white">
+      <Navbar />
+      <Box className="header h-2/5">
+        <Box
+          className="header-top mr-4"
+          sx={{ display: "flex", flexDirection: "row" }}
+        >
+          <Box className="w-6/12">
+            <img
+              src="images/logoFitness.jpg"
+              className="size-28 pl-6 object-cover"
+            />
+          </Box>
+          <Box className="flex w-6/12 place-content-end gap-x-8 items-center">
+            {/* Profile and Signin register and seach bar */}
 
+            <a href="#">Register</a>
+            <a href="#">Sign in</a>
+
+            <TextField
+              id="outlined-basic"
+              label="Search"
+              variant="outlined"
+              className="text-white"
+            />
+            <img src="./images/landingPage1.jpg" className="size-28 " />
+          </Box>
+        </Box>
+        <Box className="header-bottom h-auto flex items-center ">
+          <Stack spacing={2} direction="row" className="p-6 ml-9">
+           {/* <button className="bg-white hover:bg-red-400 text-black p-4 rounded-lg shadow-lg text-sm 
+
+"> Weight Training</button> */}
+            <Button variant="contained">Weight Training</Button>
+            <Button variant="contained">Yoga</Button>
+            <Button variant="contained">Running</Button>
+          </Stack>
+        </Box>
+      </Box>
+      {children}
+    </Box>
+  );
+};
+
+export default LayoutDesk;
