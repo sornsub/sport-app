5286d2da3da0220d4e204396547eb9c35d19cb5a
SenPatthamawan <sen.patthamwan@gmail.com>
2024-02-09 22:53:36 +0700

    Get api user profile data to show in dashboard

--- src/components/shared/Rightbar.jsx
+++ src/components/shared/Rightbar.jsx
@@ -1,18 +1,16 @@
+import React, { useState, useEffect } from 'react'
+import API from '../../api/axios';
 import Drawer from '@mui/material/Drawer';
 import Divider from '@mui/material/Divider';
-
 import { 
   Box,
   Grid,
   Typography,
   ThemeProvider,
 } from "@mui/material"
-
 import { sizing } from '@mui/system';
 
-
-
-import AvatarImg from "/images/avatar.jpg"
+import AvatarImg from "/images/default_avatar.png"
 import Forgotpassword from "/images/forgotpassword.jpg"
 import { theme } from "./../../theme"
 
@@ -20,6 +18,30 @@ import { theme } from "./../../theme"
 const drawerWidth = 310;
 
 const Rightbar = () => {
+  const [user, setUser] = useState([]);
+
+  const userRoute = "api/users";
+  const token = localStorage.getItem('token');
+  const headers = {
+      'Authorization': `Bearer ${token}`
+    }
+
+  
+  useEffect(() => {
+    getUserDataById();
+  }, []);
+
+//get user data
+const getUserDataById = async () => {
+  const user_id = '65c63d76e63824aa00b3c573'; //TODO: Change user id
+  const response = await API.get(`${userRoute}/${user_id}`, {headers: headers}); // [GET] https://localhost:5000/api/users
+  console.log("response: ", response.data.data)
+  // set user here
+  if (response.status === 200 && response.data.data) {
+    setUser(response.data.data);
+  }
+};
+
   return (
     <>
       <ThemeProvider theme={ theme }>
@@ -38,28 +60,11 @@ const Rightbar = () => {
             anchor="right"
           >
             <Divider />
-            <div class="max-w-full">
-              <img src={AvatarImg} class="object-cover h-auto max-w-full" />
+            <div className="max-w-full">
+              <img src={user.avatar!==null || user.avatar!=='undefined' ? user.avatar : AvatarImg} className="object-cover h-auto max-w-full" />
             </div>
-            {/* <Grid
-                  item
-                  xs={false}
-                  sm={4}
-                  md={7}
-                  sx={{
-                    backgroundImage: `url(${AvatarImg})`,
-                    backgroundRepeat: "no-repeat",
-                    backgroundColor: (t) =>
-                      t.palette.mode === "light"
-                        ? t.palette.grey[50]
-                        : t.palette.grey[900],
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                    width: 'auto',
-                    height: '100%',
-                  }}
-                /> */}
                 <Box sx={{height: '100%', m: 4, color: 'primary.black'}}>
+                  <input type="hidden" id="id" value={user._id} />
                   <Typography variant="h5" component="h2" 
                     sx={{
                       fontWeight: 'medium',
@@ -68,28 +73,29 @@ const Rightbar = () => {
                       mb: 2,
                       
                     }}>
-                    Hi, Bobtail Soda
+                    {`Hi, ${user.userName}`}
                   </Typography>
 
                   <Typography variant="p" component="h2" 
                     >
-                    Email: bobtailsoda@gmail.com
+                    Email: {user.email}
                   </Typography>
                   <Typography variant="p" component="h2" 
                     >
-                    Date of Birth: 12/12/2000
+                    {/* TODO: format date*/}
+                    {`Date of Birth: ${user.date_of_birth}`}
                   </Typography>
                   <Typography variant="p" component="h2" 
                     >
-                    Sex: Female
+                      {`Gender: ${user.gender}`}
                   </Typography>
                   <Typography variant="p" component="h2" 
                     >
-                    Height: 170 cm
+                      {`Height: ${user.height} cm`}
                   </Typography>
                   <Typography variant="p" component="h2" 
                     >
-                    Weight: 50 kg
+                      {`Weight: ${user.weight} kg`}
                   </Typography>
               </Box>
               <div className="mb-5 flex justify-center items-center">
@@ -100,7 +106,7 @@ const Rightbar = () => {
               <Typography variant="p" component="h2" sx={{mb: 2}}>
                 Favorite Activity
               </Typography>
-            
+            {/* TODO: waitting for activity type api */}
             <div className="flex-wrap gap-6 mb-3 flex text-black h-10 w-full">
               <div className="whitespace-nowrap p-2 rounded-4xl bg-white">
                 Weight training
