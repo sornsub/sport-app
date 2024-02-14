1bffd163f1b1ecf75a7b7b6bb56cd0957db7b10c
sornsub <150174671+sornsub@users.noreply.github.com>
2024-02-14 11:39:12 +0700

    History_Mockup_Edit_V1

--- src/pages/History/HistoryV3.jsx
+++ src/pages/History/HistoryV3.jsx
@@ -9,24 +9,19 @@ import CardMedia from '@mui/material/CardMedia';
 import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
-// import Box from '@mui/material/Box';
+import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-// import Typography from '@mui/material/Typography';
-// import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
 import Navbar from '../../components/shared/Navbar';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-// ---use import for edit start
+// ---use import for edit
+// import { useNavigate, Link } from "react-router-dom";
 
-import { Box, Container, Typography, Modal } from "@mui/material";
-
-//Component
-import EditExercise from '../Exercise/EditExercise/EditExercise.jsx';
-
-// ---use import for edit end
-
-
+// import { Box, Container, Typography, ThemeProvider, Modal } from "@mui/material"
+// import { theme } from "../../../theme"
 // ---
 import * as React from 'react';
 // import AspectRatio from '@mui/joy/AspectRatio';
@@ -40,6 +35,11 @@ import * as React from 'react';
 import API from '../../api/axios';
 // import UserCreate from './Create';
 
+//Component
+import EditExercise from '../Exercise/EditExercise/EditExercise.jsx';
+
+//Image
+import createExercise from "../../components/UploadImage/UploadImage.jsx";
 // codeHistory
 function Copyright() {
   return (
@@ -56,49 +56,29 @@ function Copyright() {
 
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
-const History = () =>  {
+
+const History3 = () =>  {
   const [reload, setReload] = useState(false);
   const [exerciseActivities, setExerciseActivities] = useState([]);
-  const [updateExerciseActivities, setUpdateExerciseActivities] = useState([]);
   const exerciseActivitiesRoute = "api/exercise-activities";
   const user_id = "65c8f4fc9aa2f67b7fbd27a7";
-
-  // For Edit Process by sent api/post SummaryData to EditExercise.jsx ;
-  const [summaryData, setSummaryData] = useState({});
-    //Modal popup
-    const [open, setOpen] = useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => {
-      setOpen(false);
-      setReload(!reload);
-    };
+  
+  // const id = "65c9db8f4f0314251c487a0a";
 
   const token = localStorage.getItem('token');  // เก็บ token  login ค้างไว้
+
+
   const headers = {
-        'Authorization': `Bearer ${token}`   // ขอ Token ก่อน
-  }
-
-  // Modal popup style
-  const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-  };
-
+      'Authorization': `Bearer ${token}`   // ขอ Token ก่อน
+    }
 
   useEffect(() => {
     getExerciseActivityByUserId();
     }, [reload]);
   
-  //get user data for show histoty 
+  //get user data
   const getExerciseActivityByUserId = async () => {
-  
+    console.log("Fromgetexercise");
    
     // const response = await API.get(`http://localhost:5173/api/exercise-activities/user/65c8f4fc9aa2f67b7fbd27a7`, {headers: headers}); 
     const response = await API.get(`${exerciseActivitiesRoute}/user/${user_id}`, {headers: headers}); // [GET] https://localhost:5000/api/exercise-activities/user/:user_id
@@ -109,45 +89,12 @@ const History = () =>  {
     }
   };
 
-  
-    //click button to edit 1-exercise-id data 
-       // get user 1-exercise-id data 
-    const getExerciseActivityById = async () => {
-
-      const response = await API.get(`${exerciseActivityRoute}/${id}`, {headers: headers}); // [GET] https://localhost:5000/api/:user_id    console.log("response: ", response.data)
-      // set member here
-      if (response.status === 200 && response.data.data) {
-        setEditExerciseActivities([...response.data.data]);
-      }
-    };
-    const updateExerciseActivity = async ({id, activity_type_id, caption, description, hour, minute, date, image}) => {
-      // const id = '65b9fced5cfcc8eb551496b6';
-      const requestData = {
-        id: id,
-        activity_type_id: activity_type_id,
-        caption: caption,
-        description: description,
-        hour: hour,
-        minute: minute,
-        date: date,
-        image: image,
-      };
-    console.log(requestData);
-    const response = await API.put(`${exerciseActivityRoute}/${id}`, requestData, {headers: headers});// [PUT] https://localhost:5000/api/users , requestData
-
-    if (response.statusText === "OK") {
-    localStorage.setItem('exercise_activity_id', response.data.data._id);
-    console.log("id from update: ",response.data.data._id);
-    setReload(!reload);//
- //   navigate("/exercise-activity/summary");
-    }
-    console.log(response);
-  };
-
   // Delete data
       const removeData = async (id) => {
         console.log("id: ", id)
-        const response = await API.delete(`${exerciseActivitiesRoute}/${id}`, {headers: headers})
+        // const response = await API.get(`${exerciseActivitiesRoute}/user/${user_id}`, {headers: headers}); // [GET] https://localhost:5000/api/exercise-activities/user/:user_id
+ 
+              const response = await API.delete(`${exerciseActivitiesRoute}/${id}`, {headers: headers})
           
               if (response.status === 200) {     // ถ้าลบสำเร็จ 105 จะช่วย reload
                 setReload(!reload);
@@ -227,10 +174,9 @@ const History = () =>  {
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16"><path fill="currentColor" fill-rule="evenodd" d="M9 2H7a.5.5 0 0 0-.5.5V3h3v-.5A.5.5 0 0 0 9 2m2 1v-.5a2 2 0 0 0-2-2H7a2 2 0 0 0-2 2V3H2.251a.75.75 0 0 0 0 1.5h.312l.317 7.625A3 3 0 0 0 5.878 15h4.245a3 3 0 0 0 2.997-2.875l.318-7.625h.312a.75.75 0 0 0 0-1.5zm.936 1.5H4.064l.315 7.562A1.5 1.5 0 0 0 5.878 13.5h4.245a1.5 1.5 0 0 0 1.498-1.438zm-6.186 2v5a.75.75 0 0 0 1.5 0v-5a.75.75 0 0 0-1.5 0m3.75-.75a.75.75 0 0 1 .75.75v5a.75.75 0 0 1-1.5 0v-5a.75.75 0 0 1 .75-.75" clip-rule="evenodd"/></svg>
                   </button>
                
-                 {/* edit */}
-                <button onClick={handleOpen} className="text-grey pr-5 flex justify-start items-center">
+                  {/* <button onClick={handleOpen} className="text-grey pr-5 flex justify-start items-center">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M21 12a1 1 0 0 0-1 1v6a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h6a1 1 0 0 0 0-2H5a3 3 0 0 0-3 3v14a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3v-6a1 1 0 0 0-1-1m-15 .76V17a1 1 0 0 0 1 1h4.24a1 1 0 0 0 .71-.29l6.92-6.93L21.71 8a1 1 0 0 0 0-1.42l-4.24-4.29a1 1 0 0 0-1.42 0l-2.82 2.83l-6.94 6.93a1 1 0 0 0-.29.71m10.76-8.35l2.83 2.83l-1.42 1.42l-2.83-2.83ZM8 13.17l5.93-5.93l2.83 2.83L10.83 16H8Z"/></svg>
-                </button>
+                  </button> */}
 
                   <Button size="small">Edit</Button>
                   </CardActions>
@@ -265,40 +211,70 @@ const History = () =>  {
             ))}
           </Grid>
         </Container>
+{/* 
+     <Card
+      size="lg"
+      variant="plain"
+      orientation="horizontal"
+      sx={{
+        textAlign: 'center',
+        maxWidth: '100%',
+        width: 393,
+        // to make the demo resizable
+        resize: 'horizontal',
+        // overflow: 'auto',
+      }}
+     >
 
-            <Modal
-                  open={open}
-                  onClose={handleClose}
-                  aria-labelledby="modal-modal-title"
-                  aria-describedby="modal-modal-description"
-                >
-                <Container
-                  sx={{
-                    // height: "100vh",
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                  }}
-                >
-                  <Box
-                    sx={{
-                      width: "872px",
-                      textAlign: "center",
-                    }}
-                  >
-                    <Typography
-                      variant="h5"
-                      component="h1"
-                      sx={{ fontWeight: "medium",
-                            m:5,
-                    }}
-                    >
-                      Update Tracking Exercise Activity
-                    </Typography>
-                    <EditExercise update={updateExerciseActivity} summaryData={summaryData} handleClose={handleClose}/>
-                  </Box>
-                </Container>
-              </Modal>
+     <CardOverflow
+        variant="solid"
+        color="primary"
+        sx={{
+          flex: '0 0 200px',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'left',
+          // px: 'var(--Card-padding)',
+        }}
+      >
+        <AspectRatio ratio="1" sx={{ width: 293 }}>
+          <img
+            src="https://images.unsplash.com/photo-1507833423370-a126b89d394b?auto=format&fit=crop&w=90"
+            srcSet="https://images.unsplash.com/photo-1507833423370-a126b89d394b?auto=format&fit=crop&w=90&dpr=2 2x"
+            loading="lazy"
+            alt=""
+          />
+        </AspectRatio>
+      </CardOverflow>
+      <CardContent sx={{ gap: 1.5, minWidth: 200 }}>
+        <AspectRatio ratio="19/8" objectFit="contain" variant="plain">
+          <img
+            alt=""
+            src="https://static.vecteezy.com/system/resources/previews/006/409/485/original/people-thinking-to-make-decision-problem-solving-and-find-creative-ideas-with-question-mark-in-flat-cartoon-background-for-poster-illustration-vector.jpg"
+          />
+        </AspectRatio>
+        <CardContent>
+          <Typography level="title-lg">Need Some Help?</Typography>
+          <Typography fontSize="sm" sx={{ mt: 0.5 }}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor.
+          </Typography>
+        </CardContent>
+        <Button
+          variant="outlined"
+          color="primary"
+          sx={{
+            '--variant-borderWidth': '2px',
+            borderRadius: 40,
+            borderColor: 'primary.500',
+            mx: 'auto',
+          }}
+        >
+          See FAQ
+        </Button>
+      </CardContent>
+    </Card> */}
+
       </main>
       {/* Footer */}
       <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
@@ -318,6 +294,8 @@ const History = () =>  {
       {/* End footer */}
     </ThemeProvider>
   );
+
+  
 }
 
-export default History
+export default History3
