d7348395c0dd7fb2ff5512b1024a949bf044ae2a
ekachai39_8re <146946429+ekachaikeaw@users.noreply.github.com>
2024-02-14 21:40:01 +0700

    add woman image

--- src/pages/LandingPage/LayoutDesk.jsx
+++ src/pages/LandingPage/LayoutDesk.jsx
@@ -6,16 +6,14 @@ import "./LandingPage.css";
 export const LayoutDesk = ({ children }) => {
   return (
     <div className="bg-pink h-full text-white">
-      <div className="flex h-[200px] justify-between">
-        <Box className="ml-2 ">
+      <div className="flex h-[200px] bg-red justify-between">
+        <Box className="ml-2 w-1/3 ">
           {/* picture */}
-          <img
-            src="images/logoRemoveBg.png"
-            className="w-full h-[200px] pl-6 object-cover"
-          />
+          <img src="images/logoRemoveBg.png" className=" pl-6 object-cover" />
         </Box>
         {/* Intro nav menu */}
-        <Box className="flex gap-x-20 items-center pr-5">
+
+        <Box className="flex gap-x-20 items-center pr-5 w-1/3">
           <a href="#">
             <strong>Service</strong>
           </a>
@@ -24,7 +22,7 @@ export const LayoutDesk = ({ children }) => {
           <a href="#">Review</a>
         </Box>
         {/* Register and Sign in button */}
-        <Box className="flex gap-x-10 items-center p-10">
+        <Box className="flex gap-x-10 items-center p-10 w-1/3">
           <button
             type="submit"
             className="shadow-lg outline-0 w-[300px] border-transparent rounded-4xl text-white block w-full p-2.5 bg-blue hover:opacity-70"
@@ -40,11 +38,18 @@ export const LayoutDesk = ({ children }) => {
         </Box>
       </div>
       {/* ปลุกใจในตัวคุณ */}
-      <div className="flex flex-col ml-40 mt-10">
-        <span className="text-7xl Font-second">BE READY</span>
-        <span className="text-7xl Font-second">AND GO </span>
-        <span className="text-7xl Font-second">FOR YOUR LIFE</span>
-      </div>
+      <Box className="flex mx-50 h-97 justify-center">
+        <div className="flex flex-col  mt-10 gap-10">
+          <span className="text-7xl Font-second">BE READY</span>
+          <span className="text-7xl Font-second">AND GO </span>
+          <span className="text-7xl Font-second">FOR YOUR LIFE</span>
+        </div>
+        <img
+          src="public\images\landing_woman-removebg.png"
+          className="object-cover"
+        />
+      </Box>
+
       {/* Swiper */}
       <Box>
         <Swipers />
