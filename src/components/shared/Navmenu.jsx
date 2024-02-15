46624c6e2e355fbe1d96392bafe4b5946eebd1b1
ekachai39_8re <146946429+ekachaikeaw@users.noreply.github.com>
2024-02-15 14:34:26 +0700

    add link

--- src/components/shared/Navmenu.jsx
+++ src/components/shared/Navmenu.jsx
@@ -7,10 +7,10 @@ export default function Navmenu() {
   return (
     <React.Fragment>
       <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
-        <Typography sx={{ minWidth: 100 ,fontSize: '1.2rem'}}>Home</Typography>
-        <Typography sx={{ minWidth: 100 ,fontSize: '1.2rem'}}>Exercise activity</Typography>
-        <Typography sx={{ minWidth: 100 ,fontSize: '1.2rem'}}>History</Typography>
-        <Typography sx={{ minWidth: 100 ,fontSize: '1.2rem'}}>Settings</Typography>
+        <a href='/'><Typography sx={{ minWidth: 100 ,fontSize: '1.2rem'}}>Home</Typography></a>
+        <a href='/exercise-activity'><Typography sx={{ minWidth: 100 ,fontSize: '1.2rem'}}>Exercise activity</Typography></a>
+        <a href='/history'><Typography sx={{ minWidth: 100 ,fontSize: '1.2rem'}}>History</Typography></a>
+        <a href=''><Typography sx={{ minWidth: 100 ,fontSize: '1.2rem'}}>Settings</Typography></a>
         
       </Box>
     </React.Fragment>
