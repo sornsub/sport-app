078ae8ea0d5fa97815610e1aecf8205541c14415
sornsub <150174671+sornsub@users.noreply.github.com>
2024-02-15 19:21:07 +0700

    History_Mockup_DeletemodolV1

--- src/pages/History/History.jsx
+++ src/pages/History/History.jsx
@@ -249,6 +249,32 @@ const History = () =>  {
   const user_id = localStorage.getItem('userId');
   const navigate = useNavigate();
   
+  // //Moral
+  // const [open, setOpen] = React.useState(false);
+  // const handleClose = () => setOpen(false);
+  // const handleOpen = () => setOpen(true);
+
+  
+  //Handle Modal popup
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+    setReload(!reload);
+  };
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+    
   // const id = "65c9db8f4f0314251c487a0a";
 >>>>>>> 3f98031 (Histoy_Mockup_View5)
 
@@ -294,7 +320,7 @@ const History = () =>  {
 >>>>>>> 9fb9e2c (History_Mockup_Edit_V1)
 =======
         // const response = await API.get(`${exerciseActivitiesRoute}/user/${user_id}`, {headers: headers}); // [GET] https://localhost:5000/api/exercise-activities/user/:user_id
- 
+  
               const response = await API.delete(`${exerciseActivitiesRoute}/${id}`, {headers: headers})
 >>>>>>> 4e4fb34 (History_Mockup_View_V1)
 =======
@@ -320,6 +346,7 @@ const History = () =>  {
               if (response.status === 200) {     // ถ้าลบสำเร็จ 105 จะช่วย reload
                  handleClose ();
                  console.log(response);
+<<<<<<< HEAD
               }
             };
 
@@ -350,6 +377,8 @@ const History = () =>  {
                 localStorage.setItem('exercise_activity_id', response.data.data._id);
                 navigate("/exercise-activity/summary");   
                 console.log(response);
+=======
+>>>>>>> d131c23 (History_Mockup_DeletemodolV1)
               }
             };
 
@@ -447,6 +476,7 @@ const History = () =>  {
                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M21 12a1 1 0 0 0-1 1v6a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h6a1 1 0 0 0 0-2H5a3 3 0 0 0-3 3v14a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3v-6a1 1 0 0 0-1-1m-15 .76V17a1 1 0 0 0 1 1h4.24a1 1 0 0 0 .71-.29l6.92-6.93L21.71 8a1 1 0 0 0 0-1.42l-4.24-4.29a1 1 0 0 0-1.42 0l-2.82 2.83l-6.94 6.93a1 1 0 0 0-.29.71m10.76-8.35l2.83 2.83l-1.42 1.42l-2.83-2.83ZM8 13.17l5.93-5.93l2.83 2.83L10.83 16H8Z"/></svg>
                       </button>
 
+<<<<<<< HEAD
                        <Button size="small">Delete</Button>
                        <button onClick={handleOpen} className="text-grey pr-5 flex justify-start items-center">
                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16"><path fill="currentColor" fill-rule="evenodd" d="M9 2H7a.5.5 0 0 0-.5.5V3h3v-.5A.5.5 0 0 0 9 2m2 1v-.5a2 2 0 0 0-2-2H7a2 2 0 0 0-2 2V3H2.251a.75.75 0 0 0 0 1.5h.312l.317 7.625A3 3 0 0 0 5.878 15h4.245a3 3 0 0 0 2.997-2.875l.318-7.625h.312a.75.75 0 0 0 0-1.5zm.936 1.5H4.064l.315 7.562A1.5 1.5 0 0 0 5.878 13.5h4.245a1.5 1.5 0 0 0 1.498-1.438zm-6.186 2v5a.75.75 0 0 0 1.5 0v-5a.75.75 0 0 0-1.5 0m3.75-.75a.75.75 0 0 1 .75.75v5a.75.75 0 0 1-1.5 0v-5a.75.75 0 0 1 .75-.75" clip-rule="evenodd"/></svg>
@@ -517,11 +547,45 @@ const History = () =>  {
                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16"><path fill="currentColor" fill-rule="evenodd" d="M9 2H7a.5.5 0 0 0-.5.5V3h3v-.5A.5.5 0 0 0 9 2m2 1v-.5a2 2 0 0 0-2-2H7a2 2 0 0 0-2 2V3H2.251a.75.75 0 0 0 0 1.5h.312l.317 7.625A3 3 0 0 0 5.878 15h4.245a3 3 0 0 0 2.997-2.875l.318-7.625h.312a.75.75 0 0 0 0-1.5zm.936 1.5H4.064l.315 7.562A1.5 1.5 0 0 0 5.878 13.5h4.245a1.5 1.5 0 0 0 1.498-1.438zm-6.186 2v5a.75.75 0 0 0 1.5 0v-5a.75.75 0 0 0-1.5 0m3.75-.75a.75.75 0 0 1 .75.75v5a.75.75 0 0 1-1.5 0v-5a.75.75 0 0 1 .75-.75" clip-rule="evenodd"/></svg>
                        </button>
  
+=======
+>>>>>>> d131c23 (History_Mockup_DeletemodolV1)
                        <Button size="small">Delete</Button>
-                       <button onClick={() => removeData(exerciseActivity._id)} className="text-grey pr-5 flex justify-start items-center">
+                       <button onClick={handleOpen} className="text-grey pr-5 flex justify-start items-center">
                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16"><path fill="currentColor" fill-rule="evenodd" d="M9 2H7a.5.5 0 0 0-.5.5V3h3v-.5A.5.5 0 0 0 9 2m2 1v-.5a2 2 0 0 0-2-2H7a2 2 0 0 0-2 2V3H2.251a.75.75 0 0 0 0 1.5h.312l.317 7.625A3 3 0 0 0 5.878 15h4.245a3 3 0 0 0 2.997-2.875l.318-7.625h.312a.75.75 0 0 0 0-1.5zm.936 1.5H4.064l.315 7.562A1.5 1.5 0 0 0 5.878 13.5h4.245a1.5 1.5 0 0 0 1.498-1.438zm-6.186 2v5a.75.75 0 0 0 1.5 0v-5a.75.75 0 0 0-1.5 0m3.75-.75a.75.75 0 0 1 .75.75v5a.75.75 0 0 1-1.5 0v-5a.75.75 0 0 1 .75-.75" clip-rule="evenodd"/></svg>
-                       </button>         
+                       </button>    
+                       {/* <button
+                         onClick={handleOpen}
+                         className="rounded-4xl text-white bg-red text-sm w-full px-5 py-2.5 text-center"
+                        >
+                         Delete Account
+                        </button> */}
+      <Modal
+        className="self-center"
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box
+          sx={style}
+          className="rounded-4xl text-black bg-red text-sm w-full px-5 py-2.5 text-center"
+        >
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            You are going to delete accout
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Are you sure about this ?
+          </Typography>
+          <input type="hidden" id="id" value={exerciseActivity} />
+          <button
+             onClick={() => removeData(exerciseActivity._id)}
+             className="rounded-4xl text-white bg-red text-sm w-full px-5 py-2.5 text-center"
+            >
+             Delete for sure
+          </button>
+          <Typography>
 
+<<<<<<< HEAD
 >>>>>>> ed42b90 (History_Mockup_View_V1)
                
                   {/* <button onClick={handleOpen} className="text-grey pr-5 flex justify-start items-center">
@@ -574,6 +638,19 @@ const History = () =>  {
 
                        <Button size="small">Edit</Button>
                      </CardActions>
+=======
+          </Typography>
+          <button
+             onClick={() => handleClose()}
+             className="rounded-4xl text-white bg-red text-sm w-full px-5 py-2.5 text-center"
+            >
+             Cancel
+          </button>
+          {/* <DeleteButtonNested /> */}
+        </Box>
+      </Modal>
+                            </CardActions>
+>>>>>>> d131c23 (History_Mockup_DeletemodolV1)
 
 >>>>>>> 3f98031 (Histoy_Mockup_View5)
                     <CardMedia
