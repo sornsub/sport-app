c434306d4298c6d17e582b787f7bb0e0049c571c
Amornrat <Amornrat.Rtn@gmail.com>
2024-02-16 20:50:25 +0700

    connect-api

--- src/pages/ActivityType/Cards.jsx
+++ src/pages/ActivityType/Cards.jsx
@@ -15,7 +15,7 @@ export default function Cards() {
                 <div className="card card-compact w-64 bg-base-100 shadow-xl">
                     <figure><img src="https://plus.unsplash.com/premium_photo-1680042813079-605c60b629f8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Running" /></figure>
                     <div className="card-body">
-                        <h2 className="card-title">Running</h2>
+                        <h2 className="card-title">Walking</h2>
                         <p>If you want to run, run a mile. if you want to experience a different life, run a marathon.</p>
                         <div className="card-actions justify-end">
                             <button className="btn" style={{backgroundColor: '#E76F6D'}}>Learn more</button>
@@ -25,7 +25,7 @@ export default function Cards() {
                 <div className="card card-compact w-64 bg-base-100 shadow-xl">
                     <figure><img src="https://plus.unsplash.com/premium_photo-1680042813079-605c60b629f8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Running" /></figure>
                     <div className="card-body">
-                        <h2 className="card-title">Running</h2>
+                        <h2 className="card-title">Swimming</h2>
                         <p>If you want to run, run a mile. if you want to experience a different life, run a marathon.</p>
                         <div className="card-actions justify-end">
                             <button className="btn" style={{backgroundColor: '#E76F6D'}}>Learn more</button>
@@ -35,7 +35,7 @@ export default function Cards() {
                 <div className="card card-compact w-64 bg-base-100 shadow-xl">
                     <figure><img src="https://plus.unsplash.com/premium_photo-1680042813079-605c60b629f8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Running" /></figure>
                     <div className="card-body">
-                        <h2 className="card-title">Running</h2>
+                        <h2 className="card-title">Weight training</h2>
                         <p>If you want to run, run a mile. if you want to experience a different life, run a marathon.</p>
                         <div className="card-actions justify-end">
                             <button className="btn" style={{backgroundColor: '#E76F6D'}}>Learn more</button>
@@ -45,7 +45,7 @@ export default function Cards() {
                 <div className="card card-compact w-64 bg-base-100 shadow-xl">
                     <figure><img src="https://plus.unsplash.com/premium_photo-1680042813079-605c60b629f8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Running" /></figure>
                     <div className="card-body">
-                        <h2 className="card-title">Running</h2>
+                        <h2 className="card-title">Yoga</h2>
                         <p>If you want to run, run a mile. if you want to experience a different life, run a marathon.</p>
                         <div className="card-actions justify-end">
                             <button className="btn" style={{backgroundColor: '#E76F6D'}}>Learn more</button>
