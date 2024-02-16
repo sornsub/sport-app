ec3f65372b275ccc7b72d977c61b3766f1578ab2
SenPatthamawan <sen.patthamwan@gmail.com>
2024-02-16 21:13:53 +0700

    Refacter code dasboard page#1

--- src/components/Charts/DonutChart.jsx
+++ src/components/Charts/DonutChart.jsx
@@ -1,37 +1,23 @@
 import React, { useState, useEffect } from 'react'
 import Chart from "react-apexcharts";
-import API from '../../api/axios';
+import DashboardAPI from '../../api/services/dashboard';
 
 import 'apexcharts/dist/apexcharts.css';
 
 const DonutChart = ({selectedRange, userId, reload}) => {
 
   const [activitiesTypeData, setActivitiesTypeData] = useState([]);
-  
-  const dashboardRoute = "api/dashboard";
-
-  const token = localStorage.getItem('token');
-
-  const headers = {
-      'Authorization': `Bearer ${token}`
-    }
 
   useEffect(() => {
     getActivitiesTypeByUserId();
   }, []);
 
-  //get activities Type Data By User Id
+  // get activities Type Data By User Id
   const getActivitiesTypeByUserId  = async () => {
-
-    const response = await API.get(`${dashboardRoute}/activities-type/${userId}`,
-                                    { params: { date_range: selectedRange } },
-                                    {headers: headers}
-                                  ); // [GET] https://localhost:5000/api/dashboard/summary-card/:id
-    // set User data here
-    if (response.data.data) {
-      setActivitiesTypeData([...response.data.data.chart_datas]);
+    const response = await DashboardAPI.getActivitiesTypeByUserId(userId, selectedRange)
+    if (response) {
+      setActivitiesTypeData([...response.chart_datas]);
     }
-
   };
 
   const seriesArray = [];
@@ -60,8 +46,6 @@ const DonutChart = ({selectedRange, userId, reload}) => {
          series={chartData.series}
          type="donut"
          width="480"
-
-        //  sx={{width: 'auto'}}
       />
   )
 }
