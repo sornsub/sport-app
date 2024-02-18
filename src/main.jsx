7d33e8b6d2f6ff957be4f41c800d763585ae00d9
Amornrat <Amornrat.Rtn@gmail.com>
2024-02-19 01:17:54 +0700

    add-activity

--- src/main.jsx
+++ src/main.jsx
@@ -33,6 +33,12 @@ import UserList from "./pages/User/List.jsx";
 import TrackingExerciseForm from "./pages/Exercise/CreateExercise/TrackingExerciseForm.jsx";
 import Swipers from "./pages/LandingPage/Swipers.jsx";
 
+import Running from "./pages/ActivityType/Running.jsx";
+import Walking from "./pages/ActivityType/Walking.jsx";
+import Yoga from "./pages/ActivityType/Yoga.jsx";
+import WeigtTraining from "./pages/ActivityType/WeightTraining.jsx";
+import InputFrom from "./pages/Authentication/SignUp/InputFrom.jsx";
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -81,6 +87,30 @@ const router = createBrowserRouter([
   {
     path: "/activity-type",
     element: <ActivityType />,
+  },
+  {
+    path: "/activity-running",
+    element: <Running />,
+  },
+  {
+    path: "/activity-walking",
+    element: <Walking />,
+  },
+  {
+    path: "/activity-swimming",
+    element: <Swimming />,
+  },
+  {
+    path: "/activity-weight-training",
+    element: <WeigtTraining />,
+  },
+  {
+    path: "/activity-yoga",
+    element: <Yoga />,
+  },
+  {
+    path: "/input-form",
+    element: <InputFrom />,
   },
   {
     path: "/exercise-create",
