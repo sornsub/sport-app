7fcd8e1376d1930e09b3ada86cd8d6fc25cd84eb
Amornrat <Amornrat.Rtn@gmail.com>
2024-02-19 01:23:10 +0700

    edit-navbar

--- src/pages/ActivityType/Nav.jsx
+++ src/pages/ActivityType/Nav.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
@@ -75,38 +74,3 @@ function Nav() {
 }
 
 export default Nav;
-=======
-import React from 'react';
-import { Typography } from '@mui/material';
-
-function Nav() {
-    return (
-        <div className="navbar" style={{backgroundColor: '#E76F6D'}} >
-            <div className="flex-1" >
-                <img style={{ width: '100px', height: 'auto', maxWidth: '100%' }} alt="logo Navbar component" src="public\images\logoRemoveBg.png" />
-            </div>
-            <div className="flex-none gap-2">
-                <div className="flex-none font-semibold text-white">
-                    <ul className="menu menu-horizontal px-2 text-base">
-                        <li><a href='/dashboard'>Home</a></li>
-                        <li><a href='/activity-type'>Classes</a></li>
-                        <li><a href='/history'>History</a></li>
-                        <li><a href='/edit-profile'>Setting</a></li>
-                    </ul>
-                </div>
-                <div className="form-control h-10">
-                    <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto rounded-full" />
-                </div>
-                <div className="btn btn-ghost btn-circle avatar">
-                    <div className="w-10 rounded-full">
-                        <img alt="Tailwind CSS Navbar component" src="public/images/avatar.jpg" />
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-}
-
-export default Nav;
-
->>>>>>> 322ca87 (ui-activity-type)
